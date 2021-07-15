b7743eb94bfb9e06d8c37b20d1148ff6bae0cf05
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-15 14:21:31 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+// Erstelle eine Variable namens carName, und weise ihr den Wert BMW zu.
+// Gib die Variable mit der Konsole aus.
+let carName = "BMW";
+console.log(carName);
+
+// Erstelle eine Variable mit dem Namen x, und weise ihr den Wert: 150 zu.
+// Gib die Variable mit der Konsole aus.
+let x = 150;
+console.log(x);
+
+// Erstelle eine Variable mit dem Namen y, und weise ihr den Wert: 50 zu.
+// Gib die Variable mit der Konsole aus.
+let y = 50;
+console.log(y);
+
+// Erstelle eine Variable mit dem Namen z, und weise ihr den Wert: x + y zu.
+// Gib die Variable mit der Konsole aus.
+let z = x + y;
+console.log(z);
+
+/*
+    Deklariere in einer einzelnen Zeile drei Variablen mit den folgenden Namen und Werten:
+    let firstName;
+    let lastName;
+    let age;
+    firstName = "John"
+    lastName = "Doe"
+    age = 35
+    Gib die Variablen mit der Konsole aus.
+*/
+let firstName = "John", lastName = "Doe", age = 35;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
